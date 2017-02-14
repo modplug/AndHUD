f4ee5eb9546dcb8e638032d0d639fc7411d92c50
Erlend Angelsen <erlend.angelsen@gmail.com>
2017-02-14 20:39:28 +0100

    Add animated progress sample

---
+++ AndHUD.Samples/MainActivity.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Threading.Tasks;
+using Android.App;
+using Android.Content.Res;
+using Android.OS;
+using Android.Widget;
+
+namespace AndHUD.Samples
+{
+    [Activity(Label = "AndHUD.Samples", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        int count = 1;
+
+        protected override async void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+            var button = FindViewById<Button>(Resource.Id.MyButton);
+            button.Click += ButtonOnClick;
+        }
+
+        private async void ButtonOnClick(object sender, EventArgs eventArgs)
+        {
+            var stream = Assets.Open("TwitterHeart.json", Access.Buffer);
+            for (int i = 0; i <= 100; i++)
+            {
+                AndroidHUD.AndHUD.Shared.ShowAnimatedProgress(this, stream, i, false, "Progress: " + i + "%");
+                await Task.Delay(50);
+                if (i == 100)
+                {
+                    AndroidHUD.AndHUD.Shared.Dismiss(this);
+                }
+            }
+        }
+    }
+}
+
